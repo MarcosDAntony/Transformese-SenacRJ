744c67e8fcdd5489160ced0c3ca5f701f4b8e5b7
MakiD <147560948+MakiiD@users.noreply.github.com>
2023-11-26 17:15:40 -0300

    oraganização das pastas

---
+++ imagens/footer.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+</head>
+<label for="user">Olá</label>
+    
+
+
+
+
+
+
+
+
+
+<script href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ imagens/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lux Medic. Uniformes</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="imagens/lux.ico" type="image/x-icon"><!-- icone da página-->
+</head>
+<body class="bg-transparent">
+    
+
+
+
+
+
+
+
+    
+
+
+  <script href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
