a277c571a96f95a2cfb8660ffef386c76ce5b5d7
MakiD <147560948+MakiiD@users.noreply.github.com>
2023-11-28 00:13:07 +0000

    .

--- codigo-fonte/footer.php
+++ codigo-fonte/footer.php
@@ -2,20 +2,14 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </head>
-<label for="user">Olá</label>
-    
+<body>
+    <footer >
 
-
-
-
-
-
-
-
-
-<script href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    </footer>
+    <script href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- codigo-fonte/header.php
+++ codigo-fonte/header.php
@@ -1,25 +1,54 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lux Medic. Uniformes</title>
+    <title>Lux Medic Uniformes</title>
+    <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="style.css">
-    <link rel="shortcut icon" href="imagens/lux.ico" type="image/x-icon"><!-- icone da página-->
+    <link rel="icon" href="/imagens/lux.ico">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap">
 </head>
-<body class="bg-transparent">
+<body>
+    <header class="card">
+    <img src="/imagens/luxmed.png" class="align-self-md-center" width="150" height="150">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid"><form class="d-flex" role="search">
+<a class="navbar-brand" href="#">LOJA ONLINE</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-expand-lg"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#"> Início </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#"> Produtos </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link disabled"> Vendas </a>
+                  </li>
+                </ul>
+                <form class="d-flex">
+                  <input class="form-control me-2" type="" placeholder="" aria-label="pesquisa">
+                  <button class="btn btn-outline-success" type="submit">Pesquisa</button>
+                </form>
+              </div>
+            </div>
+          </nav>
+      </nav>
     
+    </header>
 
+            <input class="form-control me-2" type="search">
+            <button class="btn-group w-auto h-auto end" type="submit"><img src="/imagens/lupa.png" class="h-100 w-25"></button>
+          </form></div>
 
-
-
-
-
-
+      </nav>
     
+    </header>
 
-
-  <script href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
