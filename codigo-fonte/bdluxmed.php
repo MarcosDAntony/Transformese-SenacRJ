40da3992be40f8f2d9a0d1d2971f3e1fdab45a7e
MakiD <147560948+MakiiD@users.noreply.github.com>
2023-11-29 18:57:23 +0000

    commit

--- codigo-fonte/bdluxmed.php
+++ codigo-fonte/bdluxmed.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
 <?php
 echo "Aqui vai ser o banco de dados do site!";
 
