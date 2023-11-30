48c046f08cafd4875030801eb84579b7db6ba374
MakiD <147560948+MakiiD@users.noreply.github.com>
2023-11-30 13:10:45 +0000

    commit

--- codigo-fonte/bdluxmed.php
+++ codigo-fonte/bdluxmed.php
@@ -1,17 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
 <?php
 //Aqui vai ser o banco de dados da página!
 echo 
 "Aqui é o banco de dados do site Lux Medic Uniformes! ";
 $_SESSION['usuariosBD'] = isset($_SESSION['usuariosBD']) ? $_SESSION['usuariosBD'] : [];
+
 ?>
