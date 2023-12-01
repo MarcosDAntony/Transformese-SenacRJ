89287d8e7c328402c0a5d8b66cb96bf45dde5e9b
MakiD <147560948+MakiiD@users.noreply.github.com>
2023-11-30 22:24:25 -0300

    commit

--- codigo-fonte/heade.php
+++ codigo-fonte/heade.php
@@ -6,11 +6,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="favicon" href="/imagens/icons/lux.ico">
   <title>Lux Medic Uniformes</title>
- <link rel="stylesheet" href="styles.css">
  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <img src="" alt="">
+  <img src="projeto-integrador-transformese/imagens/img_ref/luxmed.png">
 
 
     <script href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
