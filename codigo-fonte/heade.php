0c201196b5995fd0fb0842d545d52d86dec51fc0
MakiD <147560948+MakiiD@users.noreply.github.com>
2023-11-30 22:15:12 -0300

    commit

---
+++ codigo-fonte/heade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="favicon" href="/imagens/icons/lux.ico">
+  <title>Lux Medic Uniformes</title>
+ <link rel="stylesheet" href="styles.css">
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <img src="" alt="">
+
+
+    <script href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
