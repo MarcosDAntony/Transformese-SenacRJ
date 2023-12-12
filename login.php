6f5f31ace1301d1e67151fa4214ec3f3435fac95
Karinem9 <152201868+Karinem9@users.noreply.github.com>
2023-12-12 11:16:26 -0300

    Create login.php
    Login do cliente na página

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<?php
+	$conn = new PDO("mysql:host=localhost;dbname=lol", "lucasfelinto", "Lucas.4dec");
+
+	session_start();
+
+?>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<h1>Login</h1>
+	<form action="verify.php" method="POST">
+		<input type="text" name="nome" placeholder="Nome">
+		<input type="password" name="senha" placeholder="Senha">
+		<input type="submit" name="">
+	</form>
+</body>
+</html>
