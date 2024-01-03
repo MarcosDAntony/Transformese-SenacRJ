400515b9a279c3d18adb40a51d72eadaee1d0baa
MakiD <147560948+MakiiD@users.noreply.github.com>
2024-01-03 09:59:26 -0300

    commit-projeto

---
+++ site/main.php
@@ -0,0 +1,21 @@
+<!--================================================Main==========================================================================-->
+<main> 
+
+<div id="carouselExample" class="carousel slide w3-animate-left" >
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="../site/imagens/img_ref/atual/imgmain.jpg" class="d-block w-100 imgmain" alt="imgmain">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">L</span>
+  </button>
+
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">R</span>
+  </button>
+</div>
+<hr>
+</main>
