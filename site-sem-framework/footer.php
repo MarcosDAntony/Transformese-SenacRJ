80a4bf027db522a56ed10d6ffa6aeb2b324dd171
MakiD <147560948+MakiiD@users.noreply.github.com>
2024-01-06 11:11:34 -0300

    commit-projeto

---
+++ site-sem-framework/footer.php
@@ -0,0 +1,9 @@
+<!--==================================================Footer=======================================================================-->
+<footer>
+<br>
+<br>   
+<nav class="bgfooter navbar navbar-expand-md d-flex justify-content-center">
+   <p>&copy; 2023 - Todos os direitos reservados.</p>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</footer>
---
+++ site-sem-framework/header.php
@@ -0,0 +1,66 @@
+<!-- ============================================Header I=========================================================================  -->
+<header class="headertop">
+        <div class="text-black-60 icon-link-hover">
+              <i class="fa fa-phone ms-2"></i> Contatos
+          <a href="https://www.instagram.com/deluxuniformesprofissionais/" target="_blank"><i class="fa fa-instagram w3-large ms-1"></i></a>
+          <a href="https://wa.me/5521968603959" target="_blank"><i class="fa fa-whatsapp w3-large ms-1"></i></a>(+55 21 96860-3959)
+        </div>
+           <hr>
+</header>
+<!-- =============================================Header II=========================================================================-->
+    <header class="headersub">
+    <div class="navbar navbar-expand-md text-black-70">
+
+      <div class="container-fluid">
+        <a  href="principal.php" class="animate-img w3-animate-left" target="_self">
+          <img src="../site/conteudos/imagens/img_ref/atual/deluxpro-semmaquina-removebg-preview.png" class="logo" alt="logo">
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end bgmenu" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasNavbarLabel"> <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button></h5>
+          </div>
+          <div class="offcanvas-body">
+            <ul class="navbar-nav flex-grow-1 pe-3"  style="font-size: 19px;">
+
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="../site/conteudos/sobre.html"><i class="fa fa-info-circle">Sobre</i></a>
+              </li>
+
+              <li class="nav-item dropdown">
+                <a href="#Produtos" class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="true">
+                  <i class="fa fa-shopping-bag">Produtos</i>     
+                </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="../site/conteudos/feminino.html">Feminino</a></li>
+                <li><a class="dropdown-item" href="../site/conteudos/masculino.html">Masculino</a></li> 
+            </ul>
+            </li>
+
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#Conta" role="button" data-bs-toggle="dropdown" aria-expanded="true">
+                  <i class="fa fa-user">Conta</i>
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="../site/formulario/cadastro.html">  <i class="fa fa-users"></i>Cadastro</a></li>
+                  <li><a class="dropdown-item" href="../site/formulario/login.html"> <i class="fa fa-key"></i>Login</a></li>
+                  </ul>
+              </li>
+
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="../site/formulario/carrinho.html" style="font-size: 19px;"><i class="fa fa-cart-plus"></i></a>
+              </li>
+            </ul>
+            <!-- Barra de Pesquisa -->
+              <div class="divBusca">
+             <input href="#Pesquisa" type="search" class="inputBusca" placeholder=" Buscar..."/>
+               <a href=""><img src="../site/conteudos/imagens/icons/atuais/lupamenor.png" height="26" width="26"/></a>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
