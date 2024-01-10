5e168703d4d179a73fcea6a7608e497c8f4c8d31
MakiD <147560948+MakiiD@users.noreply.github.com>
2024-01-10 16:48:37 -0300

    commit-projeto

--- site/main.php
+++ site/main.php
@@ -1,7 +1,7 @@
 <!--================================================Main==========================================================================-->
 <main> 
 <img src="../site/conteudos/imagens/img_ref/atual/imgmain.jpg" class="imgmain" alt="imgmain">
-<a href="https://web.whatsapp.com/send?phone=1234567890" target="_blank">
+<a href="https://wa.me/5521968603959" target="_blank">
         <img class="whatsapp-image" src="../site/conteudos/imagens/icons/atuais/whatsapp.png" alt="WhatsApp">
     </a>
 </main>
