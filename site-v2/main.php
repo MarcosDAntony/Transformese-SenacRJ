7b8819754a5525a0d71239d8dcc3c0d861aa64f1
MakiiD <marcosantony.asp@gmail.com>
2024-01-13 14:37:19 -0300

    commit-projeto

---
+++ site-v2/main.php
@@ -0,0 +1,8 @@
+<!--================================================Main==========================================================================-->
+<main>
+<img src="../site/conteudos/imagens/img_ref/atual/imgmain.jpg" class="imgmain" alt="imgmain">
+      
+<a href="https://wa.me/5521968603959" target="_blank" class="whatsapp-image animate-img">
+<img src="../site/conteudos/imagens/icons/atuais/whatsapp.png" alt="WhatsApp">
+    </a>
+</main>
