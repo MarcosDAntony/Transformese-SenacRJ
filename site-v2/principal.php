18796d0c4a208e8ab0742235520b8dd430190eb9
MakiiD <marcosantony.asp@gmail.com>
2024-01-15 07:15:17 -0300

    commit-projeto

--- site-v2/principal.php
+++ site-v2/principal.php
@@ -4,7 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="../site-v2/conteudos/imagens/icons/atual/logodeluxunipro-remaster.ico" type="image/x-icon">
-  <link rel="stylesheet" href="../site-v2/style_principalv2.css">
+   <!-- W3schools--> <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <!-- Funções W3Schools --><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!--Style Próprio--><link rel="stylesheet" href="../site-v2/style_principalv2.css">
   <title>DeLux Uniformes Profissionais</title>
 </head>
 <body>
