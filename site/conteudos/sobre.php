3e927ff25c9bf0f5dda26a9a1bf355c3671084fa
MakiiD <marcosantony.asp@gmail.com>
2024-01-16 14:29:16 -0300

    commit-projeto

--- site/conteudos/sobre.php
+++ site/conteudos/sobre.php
@@ -2,12 +2,7 @@
 <html lang="pt-br">
 <head>
 <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.8">
-  <!-- Bootstrap-->  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-  <!-- Funções Bootstrap --> <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet"> 
-  <!-- W3schools--> <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-  <!-- Funções W3Schools --><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> 
-  <!-- FavIcon das Páginas --><link rel="shortcut icon" href="../site/imagens/icons/logodeluxunipro.ico" type="image/x-icon">
+<meta name="viewport" content="width=device-width, initial-scale=1.8"> 
   <style>
     *{
     font-family: 'sans-serif';
@@ -26,20 +21,21 @@
   <title>Sobre nós</title>
 </head>
 <body>
-<br><br><br><br><br><br>
+<br><br>
 <div>
 <h1>Sobre a Empresa</h1>
 
- <div class="caixa-sobre-empresa">
+ <div>
     <br>
-        <p>A De Lux produz os mais diversos tipos de jalecos para profissionais que trabalham em<br>
+        <p class="caixa-sobre-empresa">A De Lux produz os mais diversos tipos de jalecos para profissionais que trabalham em<br>
            hospitais, clínicas, consultórios, 
            assim como para estudantes<br> e demais profissionais que vestem jaleco para exercer sua atividade profissional.</p>
     <br>
-        <p>Produzimos também para os segmentos de bares, lanchonetes, restaurantes, limpeza<br> e para profissionais que prestam serviços.</p>
+        <p class="caixa-sobre-empresa">Produzimos também para os segmentos de bares, lanchonetes, restaurantes, limpeza<br> e para profissionais que prestam serviços.</p>
     <br>
-        <p>Nossos produtos são ideais para profissionais que priorizam conforto e qualidade.</p>
+        <p class="caixa-sobre-empresa">Nossos produtos são ideais para profissionais que priorizam conforto e qualidade.</p>
     </div>
 </div>
+<br><br>
 </body>
 </html>
--- site/principal.php
+++ site/principal.php
@@ -15,8 +15,9 @@
       <?= 
       include_once ("../site/header.php");
        include_once ("../site/main.php");
-       require_once ("../site/banco-de-dados/config.php");
+       include_once ("../site/conteudos/sobre.php");
        include_once ("../site/footer.php");
        ?>
+       <?= require_once ("../site/banco-de-dados/config.php");?>
   </body>
 </html>
