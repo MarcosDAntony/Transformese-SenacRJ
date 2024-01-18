1ce43713788cacbff03770e81c88881a67370ec5
MakiiD <marcosantony.asp@gmail.com>
2024-01-18 13:47:12 -0300

    commit-projeto

--- site/principal.php
+++ site/principal.php
@@ -12,7 +12,7 @@
     <title>DeLux Uniformes Profissionais</title>
   </head>
   <body>
-      <?= 
+      <?php 
       include_once ("../site/header.php");
        include_once ("../site/main.php");
        require_once ("../site/banco-de-dados/config.php");
