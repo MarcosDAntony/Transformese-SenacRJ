46bf0a7f1534655531274cf09b7cabdfd7bcfb87
MakiiD <marcosantony.asp@gmail.com>
2024-01-22 16:29:27 -0300

    commit-projeto

--- site/conteudos/hospitalar-masculino.php
+++ site/conteudos/hospitalar-masculino.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Uniformes Beleza Femininos</title>
+  <title>Uniformes Masculino Hospitalar</title>
   <style>
     body {
       font-family: Arial, sans-serif;
