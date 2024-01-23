07aba4dcef90b8f3a79f021136d33af9be3e6606
MakiiD <marcosantony.asp@gmail.com>
2024-01-23 09:23:30 -0300

    .

--- site/formularios/login.php
+++ site/formularios/login.php
@@ -7,6 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+   <!--FavIcon--> <link rel="shortcut icon" href="../imagens/icons/atual/cadastro.ico" type="image/x-icon">
   <title>Faça seu Login</title>
 
   <style>
