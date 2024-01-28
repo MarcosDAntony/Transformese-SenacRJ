ecdcfb8c8e809426f47583d685b58cec392ef881
RaitoSants <raphaelsantiagobt@gmail.com>
2024-01-28 16:09:50 -0300

    update cadastr

--- delux_uniformes/site/config/envio_cadastro.php
+++ delux_uniformes/site/config/envio_cadastro.php
@@ -8,19 +8,16 @@ $email = $_POST['email'];
 $senha = $_POST['senha'];
 $numero = $_POST['numero'];
 $uf = $_POST['uf'];
-$endereco = $_POST['endereco'];
 $cep = $_POST['cep'];
-$tipo_cadastro = $_POST['tipo_cadastro'];
-// Parece ser daqui mas não é  apesar de ser mesma tabela $carrinho = $_POST['carrinho'];
 
-// Inserir dados no banco de dados
-$sql = "INSERT INTO cadastros (nome, email, senha, numero, uf, endereco, cep, tipo_cadastro) VALUES ('$nome', '$email', '$senha','$numero', '$uf', '$endereco', '$cep', '$tipo_cadastro')";
+
+$sql = "INSERT INTO usuarios (nome, email, senha, numero, uf, cep, tipo_cadastro) VALUES ('$nome', '$email', '$senha','$numero', '$uf', '$cep', 'Usuario')";
 
 if ($conn->query($sql) === TRUE) {
-    header("Location: login.php");
+    header("Location: ../login.php");
 } 
 else{
-    header("Location: cadastro.php"). $conn->error;
+    header("Location: ../cadastro.php"). $conn->error;
 }
 
 // Fechar a conexão
