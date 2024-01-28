16127887899259f93e0a8ec16f0f997facfd11eb
MakiiD <marcosantony.asp@gmail.com>
2024-01-28 11:45:38 -0300

    recebo rapha

--- banco-de-dados/deluxuniformespro-bd.sql
+++ banco-de-dados/deluxuniformespro-bd.sql
@@ -1,55 +1,189 @@
-CREATE DATABASE IF NOT EXISTS deluxuniformespro-bd DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 28/01/2024 às 14:59
+-- Versão do servidor: 10.4.28-MariaDB
+-- Versão do PHP: 8.0.28
 
-USE deluxuniformespro-bd;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- Tabela cadastros
-CREATE TABLE cadastros (
-  id int(11) PRIMARY KEY AUTO_INCREMENT,
-  nome varchar(50) NOT NULL,
-  email varchar(254) NOT NULL,
-  senha varchar(16) NOT NULL,
-  tipo_cadastro enum('Usuario', 'Administrador') NOT NULL COMMENT '1 - Usuario / 2 - Administrador'
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `deluxuniformespro-bd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cadastros`
+--
+
+CREATE TABLE `cadastros` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `senha` varchar(16) NOT NULL,
+  `tipo_cadastro` enum('Usuario','Administrador') NOT NULL COMMENT '1 - Usuario / 2 - Administrador'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- Inserir dados na tabela cadastros
-INSERT INTO cadastros (nome, email, senha, tipo_cadastro) VALUES
-('Marcos', 'marcosantony.asp@gmail.com', '181143/marcola', 'Administrador'),
-('Victor', 'v@k.co', '1234', 'Usuario');
+--
+-- Despejando dados para a tabela `cadastros`
+--
 
--- Tabela usuarios
-CREATE TABLE usuarios (
-  id_usuario int(11) PRIMARY KEY,
-  endereco varchar(200) NOT NULL,
-  rua varchar(100) NOT NULL,
-  numero varchar(10) NOT NULL,
-  bairro varchar(50) NOT NULL,
-  cidade varchar(50) NOT NULL,
-  uf char(2) NOT NULL,
-  cep int(8) NOT NULL,
-  FOREIGN KEY (id_usuario) REFERENCES cadastros (id) ON DELETE CASCADE ON UPDATE CASCADE
+INSERT INTO `cadastros` (`id`, `nome`, `email`, `senha`, `tipo_cadastro`) VALUES
+(1, 'Marcos', 'marcosantony.asp@gmail.com', '181143/marcola', 'Administrador'),
+(2, 'Victor', 'v@k.co', '1234', 'Usuario');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `id_pedido` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO usuarios (id_usuario, endereco, rua, numero, bairro, cidade, uf, cep) VALUES
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produtos`
+--
+
+CREATE TABLE `produtos` (
+  `id_produto` int(11) NOT NULL,
+  `descricao` varchar(500) NOT NULL,
+  `imagem` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produto_pedido`
+--
+
+CREATE TABLE `produto_pedido` (
+  `id_produto` int(11) NOT NULL,
+  `id_pedido` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `endereco` varchar(200) NOT NULL,
+  `rua` varchar(100) NOT NULL,
+  `numero` varchar(10) NOT NULL,
+  `bairro` varchar(50) NOT NULL,
+  `cidade` varchar(50) NOT NULL,
+  `uf` char(2) NOT NULL,
+  `cep` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `endereco`, `rua`, `numero`, `bairro`, `cidade`, `uf`, `cep`) VALUES
 (1, 'Nova Iguaçu', 'Rua do Adm', '123', 'Centro', 'Rio de Janeiro', 'RJ', 2147483647),
 (2, 'Outro Endereço', 'Rua XYZ', '456', 'Bairro XYZ', 'São Paulo', 'SP', 32421434);
 
-CREATE TABLE pedidos (
-  id_pedido int(11) PRIMARY KEY AUTO_INCREMENT,
-  id_usuario int(11) NOT NULL,
-  FOREIGN KEY (id_usuario) REFERENCES usuarios (id_usuario) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+--
+-- Índices para tabelas despejadas
+--
 
-CREATE TABLE produtos (
-  id_produto int(11) PRIMARY KEY AUTO_INCREMENT,
-  descricao varchar(500) NOT NULL,
-  imagem varchar(1000) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+--
+-- Índices de tabela `cadastros`
+--
+ALTER TABLE `cadastros`
+  ADD PRIMARY KEY (`id`);
 
--- Tabela produto_pedido para relacionamento entre produtos e pedidos
-CREATE TABLE produto_pedido (
-  id_produto int(11) NOT NULL,
-  id_pedido int(11) NOT NULL,
-  PRIMARY KEY (id_produto, id_pedido),
-  FOREIGN KEY (id_produto) REFERENCES produtos (id_produto) ON DELETE CASCADE ON UPDATE CASCADE,
-  FOREIGN KEY (id_pedido) REFERENCES pedidos (id_pedido) ON DELETE CASCADE ON UPDATE CASCADE
-);
+--
+-- Índices de tabela `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`id_pedido`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Índices de tabela `produtos`
+--
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`id_produto`);
+
+--
+-- Índices de tabela `produto_pedido`
+--
+ALTER TABLE `produto_pedido`
+  ADD PRIMARY KEY (`id_produto`,`id_pedido`),
+  ADD KEY `id_pedido` (`id_pedido`);
+
+--
+-- Índices de tabela `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `cadastros`
+--
+ALTER TABLE `cadastros`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de tabela `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `id_pedido` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `produtos`
+--
+ALTER TABLE `produtos`
+  MODIFY `id_produto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD CONSTRAINT `pedidos_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Restrições para tabelas `produto_pedido`
+--
+ALTER TABLE `produto_pedido`
+  ADD CONSTRAINT `produto_pedido_ibfk_1` FOREIGN KEY (`id_produto`) REFERENCES `produtos` (`id_produto`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `produto_pedido_ibfk_2` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`id_pedido`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Restrições para tabelas `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `cadastros` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
