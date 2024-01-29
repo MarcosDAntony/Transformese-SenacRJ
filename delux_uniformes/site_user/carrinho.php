4fd04566ce4bb06ddf39578a994f8a1a79099b1b
Marcos Antonio da Silva Pereira <147560948+SmileyDevFull@users.noreply.github.com>
2024-01-29 10:33:57 -0300

    a

--- delux_uniformes/site_user/carrinho.php
+++ delux_uniformes/site_user/carrinho.php
@@ -82,10 +82,118 @@ if (isset($_GET['action']) && $_GET['action'] == 'remover_item' && isset($_GET['
         .btn-success {
             margin-top: 15px;
         }
+
+         /*======================================================== Style Header=========================================================*/
+         header {
+            background: linear-gradient(0deg, rgba(106, 255, 111, 1) 0%, green);
+            height: 180px;
+        }
+
+        .logo {
+            position: relative;
+            bottom: 15px;
+            width: 240px;
+            height: 250px;
+        }
+
+        /**Barra de Pesquisa */
+        .posicao-pesquisa {
+            position: relative;
+            right: 15px;
+        }
+
+        .divBusca {
+            border-radius: 3px;
+            width: 295px;
+            height: 32px;
+            background-color: white;
+        }
+
+        .inputBusca {
+            border-radius: 3px;
+            width: 260px;
+            height: 32px;
+            border: transparent;
+        }
+
+        .lupa {
+            width: 2px;
+            height: 2px;
+        }
+
+        /**===================== */
     </style>
 </head>
 <body>
-    <?php include("../site_user/includes/heade-user.php");?>
+     <!-- =============================================Header=========================================================================-->
+     <header>
+        <div class="navbar navbar-expand-md text-black-80 container-fluid">
+            <a href="principal-user.php" class="animate-img w3-animate-left" target="_self">
+                <img src="../conteudos/imagens/img-ref/atual/deluxpro-semmaquina-removebg-preview.png" class="logo" alt="logo">
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel"> <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button></h5>
+                </div>
+
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav flex-grow-1 pe-3" style="font-size: 17px;">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="./sobre.php">Sobre</a>
+                        </li>
+
+                        <li class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="true">
+                                Produtos
+                            </a>
+                            <?php
+
+                            include './config/connect.php';
+
+                            $sql = "SELECT DISTINCT tipo FROM produtos";
+                            $result = $conn->query($sql);
+
+                            if ($result->num_rows > 0) {
+                                echo '<ul class="dropdown-menu">';
+                                
+                                while ($row = $result->fetch_assoc()) {
+                                    $tipo = $row['tipo'];
+                                    $link = strtolower(str_replace(' ', '-', $tipo)) . '.php';
+                                    
+                                    echo '<li><a href="./conteudos/' . $link . '" class="dropdown-item">' . $tipo . '</a></li>';
+                                }
+                                echo '</ul>';
+                            } else {
+                                echo "Sem tipos de produto no momento.";
+                            }
+
+
+                            $conn->close();
+                            ?>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="./area-usuario.php">
+                              <img src="./conteudos/imagens/icons/atual/conta.png" style="height:20px; width:20px;" alt="home" >
+                            </a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="./carrinho.php">
+                            <img src="./conteudos/imagens/icons/atual/carrinho.png" style="height:20px; width:20px;" alt="carrinho">
+                          </a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </div>
+    </header>
 <div class="position-carrinho">
         <h1>Carrinho de Compras</h1>
 
