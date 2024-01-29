ceff52d0e386cbea3f81fe064c8fb63f53fe094b
RaitoSants <raphaelsantiagobt@gmail.com>
2024-01-29 11:17:17 -0300

    esqueci minha senha

--- delux_uniformes/site/config/action_esqueci_senha.php
+++ delux_uniformes/site/config/action_esqueci_senha.php
@@ -1,18 +1,20 @@
 <?php
 
 
-$email = filter_input(INPUT_GET,"email", FILTER_SANITIZE_EMAIL);
+$email = filter_input(INPUT_POST,"email", FILTER_SANITIZE_EMAIL);
 $nova_senha = filter_input(INPUT_POST,"nova_senha");
-$nova_senha = hash('sha256', $nova_senha);
 
-$connection = new mysqli("localhost","root","","deluxuniformespro-bd");
+$connection = new mysqli("localhost","root","","delux_uniformes");
 
 if (!empty($email) && $_SERVER["REQUEST_METHOD"] == "POST") {
     $email = mysqli_real_escape_string($connection, $email);
 
     $query = "UPDATE usuarios SET senha = '$nova_senha' where email = '$email'";
 
-    mysqli_query($connection, $query);
+    if(mysqli_query($connection, $query)){
+        echo "Senha alterada com sucesso. Você será redirecionado em 3 segundos...";
+        header("Refresh: 3; URL=../login.php");
+    }
 
 }
 
