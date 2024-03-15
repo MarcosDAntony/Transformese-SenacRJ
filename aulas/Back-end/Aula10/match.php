6db34d4e39991eacb16f50dde3d1aae99d4211db
Marcos Antonio da Silva Pereira <147560948+SmileyDevFull@users.noreply.github.com>
2024-03-14 22:27:25 -0300

    correção da aula 10

--- aulas/Back-end/Aula10/match.php
+++ aulas/Back-end/Aula10/match.php
@@ -1,13 +1,27 @@
 <?php
-    $escolha = (int) readline("Escolha uma das opções do Menu:");
-    echo match ($escolha){
-        1 => casI(),
-        2 => casII(),
-        3 => casIII(),
-        4 => casIV(),
-        5 => casV(),
-        99 => $sair = true
-        default => ("Escolha Inválida \n")
-    }
+$escolha = (int) readline("Escolha uma das opções do Menu:");
+
+switch ($escolha) {
+    case 1:
+        echo ("Você escolheu a opção 1");
+        break;
+    case 2:
+        echo ("Você escolheu a opção 2");
+        break;
+    case 3:
+        echo ("Você escolheu a opção 3");
+        break;
+    case 4:
+        echo ("Você escolheu a opção 4");
+        break;
+    case 5:
+         echo ("Você escolheu a opção 5");
+        break;
+    case 99:
+        $sair = true;
+        break;
+    default:
+        echo "Opção inválida.";
+}
 
 ?>
